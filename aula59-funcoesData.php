094c2aec912780721a33f5152a3569c90d19c57b
Josue23 <josuerodrigues@gmail.com>
2016-12-24 23:10:55 -0200

    aula59-funcoesData.php

---
+++ aula59-funcoesData.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Funções de Data e Tempo com a função date</title>
+</head>
+<body>
+    <!-- http://membros.phpdozeroaoprofissional.com.br/home/video/59  -->
+   <h1>Funções de Data e Tempo com a função <a href="http://php.net/manual/en/function.date.php">date</a></h1>
+
+<?php
+    // dia da semana por extenso, com três letras
+    $diaSemana_D = date("D");
+
+    // data de hoje  (numérico) com zero à esquerda
+    $dataAtual_d = date("d");
+
+    // data de hoje (numérico) sem zero à esquerda
+    $dataAtual_j = date("j");
+
+    // dia do mês, ordinal  (1º, 2º, ...)
+    $dataAtual_S = date("S");
+
+    // mês atual por extenso, nome completo
+    $mesAtual_F = date("F");
+
+    echo "Today is the $dataAtual_d$dataAtual_S day of $mesAtual_F " . "<br><br>";
+
+
+    ?>
+
+    <br>
+    <br>
+
+    <a href="http://php.net/manual/en/function.date.php" target="_blank">Documentação da função date:
+    <br>
+    http://php.net/manual/en/function.date.php
+    </a>
+
+</body>
+</html>

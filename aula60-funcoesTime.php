1d79c037b2bc56b8aeaf47b27d1fb64abf8c0810
josue23 <josuerodrigues@gmail.com>
2016-12-26 02:23:14 -0200

    aula60-funcoesTime.php

--- aula60-funcoesTime.php
+++ aula60-funcoesTime.php
@@ -8,12 +8,18 @@
 	<!-- http://membros.phpdozeroaoprofissional.com.br/home/video/60 -->
 	<h1>Funções de Data e Tempo com a função <a href="http://php.net/time" target="_blank">Time</a></h1>
 	<a href="http://php.net/mktime" target="_blank">Função mktime</a><br><br>
-	<a href="http://php.net/strtotime" target="_blank">Função strtotime</a><br><br>
+	<a href="http://php.net/strtotime" target="_blank">Função strtotime - converte string em tempo</a><br><br>
 
 	<?php
+      $nextDate = date("d/m/Y", strtotime("+ 10 days"));
       $totalSegundos = time();
       echo "<hr/><br/>";
-      echo  "Total de segundos desde 01/01/1970 até agora: $totalSegundos" . "<br/><br/>";
+      echo  "Total de segundos desde 01/01/1970 até agora com a função time(): $totalSegundos" . "<br/><br/>";
+
+      echo "Total de segundos desde 01/01/1970 até agora com a função  strtotime('now') " . strtotime("now") . "<br/><br/>";
+
+      echo "Prazo de dez dias a partir de hoje é: $nextDate <br/>";
+
 
       echo "<hr/>";
 

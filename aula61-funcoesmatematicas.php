5e37101b14f63390b67d7b35897708f8a3be28ae
josue23 <josuerodrigues@gmail.com>
2016-12-26 23:49:24 -0200

    aula61-funcoesmatematicas.php

--- aula61-funcoesmatematicas.php
+++ aula61-funcoesmatematicas.php
@@ -10,38 +10,38 @@
     Função <a href="http://php.net/manual/en/function.abs.php" target="_blank">abs</a> (valor absoluto)<br>
     <?php
     echo "O valor absoluto de -5 é: " . abs(-5) . "<br/><br/>";
-     ?>
+    ?>
 
 
-     Função <a href="http://php.net/manual/en/function.round.php" target="_blank">round</a> (arredonda)<br>
-     <?php 
-     echo "O valor arredondado de 2.5 é: " . round(2.5) . "<br/><br/>" ;
-      ?>
+    Função <a href="http://php.net/manual/en/function.round.php" target="_blank">round</a> (arredonda)<br>
+    <?php 
+    echo "O valor arredondado de 2.5 é: " . round(2.5) . "<br/><br/>" ;
+    ?>
 
 
-      Função <a href="http://php.net/manual/en/function.ceil.php" target="_blank">ceil</a> (arredonda para cima)<br>
-      <?php 
-      echo "O valor de 5.3 arredondado para cima com a função ceil é: " . ceil(5.3) . "<br/><br/>";
-       ?>
+    Função <a href="http://php.net/manual/en/function.ceil.php" target="_blank">ceil</a> (arredonda para cima)<br>
+    <?php 
+    echo "O valor de 5.3 arredondado para cima com a função ceil é: " . ceil(5.3) . "<br/><br/>";
+    ?>
 
 
-      Função <a href="http://php.net/manual/en/function.floor.php" target="_blank">floor</a> (arredonda para baixo) <br>
-      <?php 
-      echo "O valor de 5.8 arredondado para baixo com a função floor é: " . floor(5.8) . "<br><br>";
-       ?>
+    Função <a href="http://php.net/manual/en/function.floor.php" target="_blank">floor</a> (arredonda para baixo) <br>
+    <?php 
+    echo "O valor de 5.8 arredondado para baixo com a função floor é: " . floor(5.8) . "<br><br>";
+    ?>
 
 
-       Função  <a href="http://php.net/manual/en/function.rand.php" target="_blank">rand</a> (número randômico)<br>
-       <span>me dar um número aleatório entre dois números, incluindo os dois números.</span><br>
-       <?php 
-       echo "Número aleatório entre 0 e 9: <br/>" . rand(0, 9) . "<br/><br/>";
+    Função  <a href="http://php.net/manual/en/function.rand.php" target="_blank">rand</a> (número randômico)<br>
+    <span>me dar um número aleatório entre dois números, incluindo os dois números.</span><br>
+    <?php 
+    echo "Número aleatório entre 0 e 9: <br/>" . rand(0, 9) . "<br/><br/>";
 
 
-       echo 'Uso da função rand para sortear nomes contidos em array("João", "Maria", "Ayumi", "Ami") <br/>';
-      $nomes = array("João", "Maria", "Ayumi", "Ami");
-      $nome = rand(0, 3);
-      echo "Sorteado: $nomes[$nome]<br/><br/>";
-        ?>
+    echo 'Uso da função rand para sortear nomes contidos em array("João", "Maria", "Ayumi", "Ami") <br/>';
+    $nomes = array("João", "Maria", "Ayumi", "Ami");
+    $nome = rand(0, 3);
+    echo "Sorteado: $nomes[$nome]<br/><br/>";
+    ?>
 
 
   </body>

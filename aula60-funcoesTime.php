1c969db8f09b588820b050ec059fa352d99b463d
josue23 <josuerodrigues@gmail.com>
2016-12-26 22:20:35 -0200

    aula60-funcoesTime.php

--- aula60-funcoesTime.php
+++ aula60-funcoesTime.php
@@ -11,19 +11,38 @@
 	<a href="http://php.net/strtotime" target="_blank">Função strtotime - converte string em tempo</a><br><br>
 
 	<?php
+
       $nextDate = date("d/m/Y", strtotime("+ 10 days"));
-      $totalSegundos = time();
+      $totalSegundos = time();    // horário atual em segundos
       echo "<hr/><br/>";
-      echo  "Total de segundos desde 01/01/1970 até agora com a função time(): $totalSegundos" . "<br/><br/>";
+      ?>
 
-      echo "Total de segundos desde 01/01/1970 até agora com a função  strtotime('now') " . strtotime("now") . "<br/><br/>";
+      $totalSegundos = time(); <br>
+      date('d/m/Y', strtotime("+2 days", $totalSegundos)) <br>
+      <?php echo date('d/m/Y', strtotime("+2 days", $totalSegundos)) . "<br/><br/><br/>"; ?>
 
-      echo "Prazo de dez dias a partir de hoje é: $nextDate <br/>";
+    <?php
+      // função time();
+      echo "Total de segundos desde 01/01/1970 até agora com a função "?>
+      <a href="http://php.net/time" target="_blank">time()</a><br>
+      <?php echo $totalSegundos ?>
+      <br/><br/><br/>
+
+      <?php echo "Prazo de dez dias a partir de hoje com as funções:<br/>"?>
+      <a href="http://php.net/manual/en/function.date.php" target="_blank">date()</a> e 
+      <a href="http://php.net/manual/en/function.strtotime.php" target="_blank">strtotime()</a>
+      <?php echo $nextDate ; ?>
+      <br/><br/><br/>
+
+      <!-- função strtotime('now'); -->
+      <?php echo "Total de segundos desde 01/01/1970 até agora com a função "?>
+      <a href="http://php.net/strtotime" target="_blank">strtotime('now')</a><br>
+      <?php echo strtotime("now") ?>
+      <br/><br/><br/>
 
 
-      echo "<hr/>";
+      <hr/>
 
-	?>
     
 </body>
 </html>
---
+++ aula61-funcoesmatematicas.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>funções matemáticas</title>
+  </head>
+  <body>
+    <!-- http://membros.phpdozeroaoprofissional.com.br/home/video/61 -->
+    <h1>Aula 61 - funções matemáticas</h1>
+    <?php
+    echo "Absoluto de -5 é " . abs(-5) . "<br/><br/>";
+     ?>
+     <a href="http://php.net/manual/en/function.abs.php" target="_blank">Função abs</a>
+
+  </body>
+</html>

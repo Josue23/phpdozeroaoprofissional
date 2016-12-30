7d5907dca71015686e58980797a077160c1032be
Josue23 <josuerodrigues@gmail.com>
2016-12-29 23:54:13 -0200

    aula68-introducaoBancoDados.php

---
+++ aula68-introducaoBancoDados.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Aula 68 - criando a estrutura d banco de dados</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+    <h1><a href="http://membros.phpdozeroaoprofissional.com.br/home/video/68" target="_blank">Aula 68</a> criando a estrutura d banco de dados</h1>
+    <!-- https://www.digitalocean.com/community/tutorials/how-to-install-and-secure-phpmyadmin-on-ubuntu-16-04  -->
+
+    <div>
+    <hr>
+    <h3>Nome do banco:</h3>
+        blog <br><br>
+
+    <h3>nome da tabela</h3>
+        posts <br><br>
+
+        campos da tabela <br>
+    <ol>
+        <li>1 - id do post, o campo id deve ser marcado como PRIMARY no campo índice e deve ser marcado o auto increment</li>
+        <li>2 - título</li>
+        <li>3 - data_criado</li>
+        <li>4 - corpo</li>
+        <li>5 - autor</li>
+    </ol>
+
+    <hr>
+    </div>
+<body>
+    
+</body>
+</html>

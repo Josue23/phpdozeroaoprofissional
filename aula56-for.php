0c8a79965887c63f04fd2a0876a6d7eed900d4d7
Josue23 <josuerodrigues@gmail.com>
2017-03-01 23:29:36 -0300

    aula56-for.php

--- aula56-for.php
+++ aula56-for.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Aula de for loop</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <!-- http://membros.phpdozeroaoprofissional.com.br/home/video/56 -->
-    <h1>Aula de for loop</h1>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula de for loop</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <!-- http://membros.phpdozeroaoprofissional.com.br/home/video/56 -->
+        <h1>Aula de for loop</h1>
 
-    <?php
+        <?php
 
-    for( $x = 0; $x < 10; $x++) {
-        echo "X é igual a $x" . "<br/>";
-    }
+        for( $x = 0; $x < 10; $x++) {
+            echo "X é igual a $x" . "<br/>";
+        }
 
-    ?>
+        ?>
 
-</body>
+    </body>
 </html>
